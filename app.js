8ee000419b9ccc7ce1d779463d63eb0778eb178b
markaa10 <rahulrajdahal@gmail.com>
2020-08-08 17:05:26 +0545

    firstComplete

---
+++ app.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    document.getElementById("birthdayMpThree").play();
+};
+
+function stopMusic() {
+    document.getElementById("birthdatMpThree").pause();
+}
+
+function playMusic() {
+    document.getElementById("birthdatMpThree").play();
+}
